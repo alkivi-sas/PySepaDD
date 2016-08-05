91975dcf6dbada7bfbd7874e9668758b87e9d620
Alkivi <admin@alkivi.fr>
2016-08-05 12:47:48 +0200

    mend

--- PySepaDD.py
+++ PySepaDD.py
@@ -142,8 +142,8 @@ class PySepaDD(object):
                 PmtInf_nodes['BIC_CdtrAgt_Node'].text = self._config['BIC']
 
             PmtInf_nodes['ChrgBrNode'].text = "SLEV"
-            PmtInf_nodes['Nm_CdtrSchmeId_Node'].text = escape(
-                                                       self._config['name'])
+            #PmtInf_nodes['Nm_CdtrSchmeId_Node'].text = escape(
+            #                                           self._config['name'])
             PmtInf_nodes['Id_Othr_Node'].text = self._config['creditor_id']
             PmtInf_nodes['PrtryNode'].text = "SEPA"
 
@@ -284,7 +284,7 @@ class PySepaDD(object):
             ED['BIC_CdtrAgt_Node'] = ET.Element("BIC")
         ED['ChrgBrNode'] = ET.Element("ChrgBr")
         ED['CdtrSchmeIdNode'] = ET.Element("CdtrSchmeId")
-        ED['Nm_CdtrSchmeId_Node'] = ET.Element("Nm")
+        #ED['Nm_CdtrSchmeId_Node'] = ET.Element("Nm")
         ED['Id_CdtrSchmeId_Node'] = ET.Element("Id")
         ED['PrvtIdNode'] = ET.Element("PrvtId")
         ED['OthrNode'] = ET.Element("Othr")
@@ -356,8 +356,8 @@ class PySepaDD(object):
 
         PmtInf_nodes['PmtInfNode'].append(PmtInf_nodes['ChrgBrNode'])
 
-        PmtInf_nodes['CdtrSchmeIdNode'].append(
-                                        PmtInf_nodes['Nm_CdtrSchmeId_Node'])
+        #PmtInf_nodes['CdtrSchmeIdNode'].append(
+        #                                PmtInf_nodes['Nm_CdtrSchmeId_Node'])
         PmtInf_nodes['OthrNode'].append(PmtInf_nodes['Id_Othr_Node'])
         PmtInf_nodes['SchmeNmNode'].append(PmtInf_nodes['PrtryNode'])
         PmtInf_nodes['OthrNode'].append(PmtInf_nodes['SchmeNmNode'])
@@ -471,8 +471,8 @@ class PySepaDD(object):
                 PmtInf_nodes['BIC_CdtrAgt_Node'].text = self._config['BIC']
 
             PmtInf_nodes['ChrgBrNode'].text = "SLEV"
-            PmtInf_nodes['Nm_CdtrSchmeId_Node'].text = escape(
-                                                       self._config['name'])
+            #PmtInf_nodes['Nm_CdtrSchmeId_Node'].text = escape(
+            #                                           self._config['name'])
             PmtInf_nodes['Id_Othr_Node'].text = self._config['creditor_id']
             PmtInf_nodes['PrtryNode'].text = "SEPA"
 
@@ -513,8 +513,8 @@ class PySepaDD(object):
 
             PmtInf_nodes['PmtInfNode'].append(PmtInf_nodes['ChrgBrNode'])
 
-            PmtInf_nodes['CdtrSchmeIdNode'].append(
-                                           PmtInf_nodes['Nm_CdtrSchmeId_Node'])
+            #PmtInf_nodes['CdtrSchmeIdNode'].append(
+            #                               PmtInf_nodes['Nm_CdtrSchmeId_Node'])
             PmtInf_nodes['OthrNode'].append(PmtInf_nodes['Id_Othr_Node'])
             PmtInf_nodes['SchmeNmNode'].append(PmtInf_nodes['PrtryNode'])
             PmtInf_nodes['OthrNode'].append(PmtInf_nodes['SchmeNmNode'])
